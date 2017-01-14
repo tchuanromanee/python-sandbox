12f38b566defe7447ea459efa3b0922aa1d7e5dd
tchuanromanee <tchuanromanee@yahoo.com>
2017-01-13 21:28:56 -0500

    Add conditionals file

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+def main():
+    x, y=100, 100 #x = 10, y = 100
+
+    if (x < y):
+        msg = "x is less than y"
+    elif (x > y):
+        msg = "x is greater than y"
+    else:
+        msg = "x is equal to y"
+
+    print(msg)
+
+main()
