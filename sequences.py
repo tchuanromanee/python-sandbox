37b01e8eeedb71454e988b7ea618fe18e98ff8eb
tchuanromanee <tchuanromanee@yahoo.com>
2017-01-19 20:33:32 -0500

    add sequences.py file

---
+++ sequences.py
@@ -0,0 +1,14 @@
+def main():
+
+    # length
+    print(len("pizza")) # print the length of string 'pizza'
+    print(len([1, 2, 3, 4, 5, 6, 7, 8])) # Length of array
+    print(len(['a', 'e', 'i', 'o', 'u']))
+
+    # maximum value
+    print(max([4, 9, 15, 3]))
+
+    # minimum value
+    print(min([3, 9, 15, 3]))
+
+main()
