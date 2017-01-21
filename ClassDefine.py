5b903cc23abfdc74603358b1b9e8a71a263e47e5
tchuanromanee <tchuanromanee@yahoo.com>
2017-01-21 10:28:58 -0500

    add ClassDefine.py

---
+++ ClassDefine.py
@@ -0,0 +1,17 @@
+# Declare a class called 'data'
+class data:
+    def create(self): # Constructor
+        print("Creating data")
+
+    def update(self): # Mutator
+        print("Updating data")
+
+    def delete(Self): # Deletion of data
+        print("Removing data")
+
+customerData = data() # Create an instance of data class caleld custmerData
+customerData.create() # Call create method of data class
+customerData.update() # Call update method of data class
+customerData.delet() # Delete customerData
+
+counter = 0 # Create an instance of the integer class
